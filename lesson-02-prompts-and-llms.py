56e550c470b60b5af7d8284bc22031ce14d30f01
Pankaj Jha <jha3@llnl.gov>
2025-02-15 00:34:00 -0800

    Add notebook and update the script for prompts and LLMs

--- lesson-02-prompts-and-llms.py
+++ lesson-02-prompts-and-llms.py
@@ -1,13 +1,72 @@
-# Prompts and LLMs
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Prompts and LLMs
+
+# In[1]:
+
+
 import os
+
+
+# In[12]:
+
 
 os.environ["OPENAI_API_KEY"] = "..."
 
+
+# In[3]:
+
+
 from langchain.llms import OpenAI
 
-llm = OpenAI(temperature=0)
+
+# ## Deterministic Response
+
+# In[4]:
+
+
+llm = OpenAI(temperature=0) # Default model is text-davinci-003
+
+
+# In[5]:
+
+
 prompt = "What would a good company name be for a company that makes colorful socks?"
 
-result = llm.generate([prompt] * 5)
+
+# In[6]:
+
+
+print(llm(prompt))
+
+
+# In[7]:
+
+
+result = llm.generate([prompt]*5)
+
+
+# In[8]:
+
+
+result
+
+
+# In[9]:
+
+
 for company_name in result.generations:
     print(company_name[0].text)
+
+
+# ## Probabilistic Response
+
+# In[10]:
+
+
+llm = OpenAI(temperature=0.9)
+result = llm.generate([prompt]*5)
+for company_name in result.generations:
+    print(company_name[0].text)
+
