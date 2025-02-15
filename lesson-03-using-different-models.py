d623d424798fcd9018a0e54b9a94e1effb75970f
Pankaj Jha <jha3@llnl.gov>
2025-02-15 02:16:47 -0800

    Add notebook and update the script for using different models (HF) with LangChain

--- lesson-03-using-different-models.py
+++ lesson-03-using-different-models.py
@@ -1,13 +1,86 @@
-# Using different models with LangChain
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Using different models with LangChain
+
+# In[1]:
+
+
 import os
 
-os.environ["OPENAI_API_KEY"] = "..."
+
+# In[11]:
+
+
+os.environ['OPENAI_API_KEY'] = "..."
+
+
+# In[12]:
+
+
+HUGGINGFACEHUB_API_TOKEN = "..."
+
+
+# In[4]:
+
 
 from langchain import HuggingFaceHub
 
-llm = HuggingFaceHub(
-    repo_id="google/flan-t5-base", model_kwargs={"temperature": 0, "max_length": 64}
-)
+
+# In[5]:
+
+
+from langchain_huggingface import HuggingFaceEndpoint
+
+
+# In[6]:
+
+
 prompt = "What are good fitness tips?"
 
+
+# ## Temperature = 0.1
+
+# In[7]:
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/flan-t5-base",
+    task="text-generation", 
+    temperature= 0.1, 
+    max_length= 64, 
+    huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
+)
+
+
+# In[8]:
+
+
 print(llm(prompt))
+
+
+# ## Temperature = 0.9
+
+# In[9]:
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/flan-t5-base",
+    task="text-generation", 
+    temperature= 0.9, 
+    max_length= 64, 
+    huggingfacehub_api_token=HUGGINGFACEHUB_API_TOKEN,
+)
+
+
+# In[10]:
+
+
+print(llm(prompt))
+
+
+# In[ ]:
+
+
+
+
