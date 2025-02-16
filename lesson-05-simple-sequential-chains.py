6b501d09352b9e9e21981f56e6b7f12491fa3ab4
Pankaj Jha <jha3@llnl.gov>
2025-02-16 00:26:45 -0800

    Add notebook and update the script for Simple Sequential Chains

--- lesson-05-simple-sequential-chains.py
+++ lesson-05-simple-sequential-chains.py
@@ -1,20 +1,74 @@
-# Simple Sequential Chains
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Simple Sequential Chains
+
+# In[1]:
+
+
 import os
 
-os.environ["OPENAI_API_KEY"] = "..."
+
+# In[10]:
+
+
+os.environ["OPENAI_API_KEY"] = ""
+
+
+# In[3]:
+
 
 from langchain import OpenAI, LLMChain, PromptTemplate
 from langchain.chains import SimpleSequentialChain
 
+
+# ### Define LLM
+
+# In[4]:
+
+
 llm = OpenAI(temperature=0)
-template = "What is a good name for a company that makes {product}?"
-first_prompt = PromptTemplate.from_template(template)
+
+
+# ### First Template
+
+# In[5]:
+
+
+first_template = "What is a good name for a company that makes {product}?"
+first_prompt = PromptTemplate.from_template(first_template)
 first_chain = LLMChain(llm=llm, prompt=first_prompt)
+
+
+# In[6]:
+
+
+print(first_chain.run("colorful socks"))
+
+
+# ### Second Template
+
+# In[7]:
+
 
 second_template = "Write a catch phrase for the following company: {company_name}"
 second_prompt = PromptTemplate.from_template(second_template)
 second_chain = LLMChain(llm=llm, prompt=second_prompt)
 
+
+# In[8]:
+
+
+#print(second_chain.run("colorful socks"))
+#print(second_chain.run("ABC Startup"))
+
+
+# ### Overall Chain
+
+# In[9]:
+
+
 overall_chain = SimpleSequentialChain(chains=[first_chain, second_chain], verbose=True)
 catchphrase = overall_chain.run("colorful socks")
 print(catchphrase)
+
