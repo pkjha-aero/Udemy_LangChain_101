03eb5286663c8d368e8dbe68c0576970c7f0c5e1
Pankaj Jha <jha3@llnl.gov>
2025-02-16 01:25:50 -0800

    Add notebook and update the script for Action Agents

--- lesson-06-action-agents.py
+++ lesson-06-action-agents.py
@@ -1,15 +1,66 @@
-# Action Agents
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Action Agents
+
+# In[1]:
+
+
 import os
 
-os.environ["OPENAI_API_KEY"] = "..."
 
+# In[2]:
+
+
+#os.environ["OPENAI_API_KEY"] = "..."
+
+
+# In[4]:
+
+
+import pprint
 from langchain.llms import OpenAI
-from langchain.agents import load_tools, initialize_agent
+from langchain.agents import load_tools, get_all_tool_names, initialize_agent
+#from langchain_community.agent_toolkits.load_tools import get_all_tool_names
+
+
+# In[5]:
+
 
 prompt = "When was the 3rd president of the United States born? What is that year raised to the power of 3?"
 
+
+# In[6]:
+
+
+pp = pprint.PrettyPrinter(indent = 4)
+pp.pprint(get_all_tool_names())
+
+
+# ### Define LLM
+
+# In[7]:
+
+
 llm = OpenAI(temperature=0)
+
+
+# ### Define Tools and Agent
+
+# In[8]:
+
+
 tools = load_tools(["wikipedia", "llm-math"], llm=llm)
+
+
+# In[9]:
+
+
 agent = initialize_agent(tools, llm, agent="zero-shot-react-description", verbose=True)
 
+
+# In[10]:
+
+
 agent.run(prompt)
+
